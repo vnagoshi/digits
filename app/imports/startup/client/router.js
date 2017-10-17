a021bd7368331cf16dd1c22545bd250709f9ef0c
Vincent Nagoshi <vnagoshi@hawaii.edu>
2017-10-16 23:17:25 -1000

    add-edit-mockups-1

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -29,6 +29,20 @@ FlowRouter.route('/stuff/:_id', {
   },
 });
 
+FlowRouter.route('/add-contact', {
+  name: 'Add_Contact_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Add_Contact_Page' });
+  },
+});
+
+FlowRouter.route('/edit-contact/:id', {
+  name: 'Edit_Contact_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Edit_Contact_Page' });
+  },
+});
+
 FlowRouter.notFound = {
   action() {
     BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
